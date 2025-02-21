32a8f466c9eb4c7fcf06e64aca39875aeaf0422b
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-02-21 12:18:42 +0100

    Creating function mostrar

--- module.js
+++ module.js
@@ -2,7 +2,12 @@
 let usuarios = [];
 
 
-// this module function adds a new name to the list "usuarios".
+// This module function adds a new name to the list "usuarios".
 function agregar(nombre) {
     usuarios.push(nombre);
+}
+
+// This module function displays the content of the list "usuarios".
+function mostrar(lista) {
+    console.log(lista);
 }
